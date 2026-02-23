6012b187d0ba8f02ef8dcafdd0661b25a6f9e9e5
FedorMerenkov <worend324@gmail.com>
2026-02-23 14:13:41 +0400

    demo

--- src/main/java/com/example/project_games/ProjectGamesApplication.java
+++ src/main/java/com/example/project_games/ProjectGamesApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//nev line
 public class ProjectGamesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectGamesApplication.class, args);
-    }
+    } //edit
 
 }
