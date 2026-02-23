c7b3e6c4278a727b3f2815b28db7f8057957333d
FedorMerenkov <worend324@gmail.com>
2026-02-23 14:23:41 +0400

    demo

---
+++ src/main/java/com/example/project_games/a.java
@@ -0,0 +1,13 @@
+package com.example.project_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class a {
+
+    public static void main(String[] args) {
+        SpringApplication.run(a.class, args);
+    }// push
+
+}
