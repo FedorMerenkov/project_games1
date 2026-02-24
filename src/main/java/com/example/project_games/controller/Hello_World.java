7dc1a9ebc3165e10ffba6bdf7915875da0660b6a
FedorMerenkov <worend324@gmail.com>
2026-02-24 22:16:41 +0400

    feat: add controller endpoint

--- src/main/java/com/example/project_games/controller/Hello_World.java
+++ src/main/java/com/example/project_games/controller/Hello_World.java
@@ -1,11 +1,9 @@
 package com.example.project_games.controller;
 
-
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 @RestController
-public class HelloWorldController {
+public class Hello_World {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World";
