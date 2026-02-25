b83481d828353dcc1ea12369705ede8f8603ca31
FedorMerenkov <worend324@gmail.com>
2026-02-25 08:03:17 +0400

    fix: controller endpoint

--- src/main/java/com/example/project_games/controller/HelloWorldController.java
+++ src/main/java/com/example/project_games/controller/HelloWorldController.java
@@ -2,10 +2,11 @@ package com.example.project_games.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
-public class Hello_World {
+public class HelloWorldController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World";
+        return "Hello.World";
     }
 }
