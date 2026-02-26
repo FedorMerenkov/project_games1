dd6767e23b5559478d6e0bc542775719f8e7b393
FedorMerenkov <worend324@gmail.com>
2026-02-27 00:03:31 +0400

    Merge pull request #2
    * Revert "deps: add spring-boot-starter-web"
    
    * deps: add spring-boot-starter-web
    
    * feat: add hello world endpoint
    
    * feat: add controller endpoint
    
    * feat: add controller endpoint
    
    * fix: controller endpoint
    
    * fix: controller endpoint

---
+++ src/main/java/com/example/project_games/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.project_games.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
